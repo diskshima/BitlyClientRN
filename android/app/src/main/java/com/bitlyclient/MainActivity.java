9eb73fac585a943d08a54a9f189d9fbe6f9881c9
Daisuke Shimamoto <diskshima@gmail.com>
2016-05-14 17:21:48 +0900

    Add feature to accept URLs back in

--- android/app/src/main/java/com/bitlyclient/MainActivity.java
+++ android/app/src/main/java/com/bitlyclient/MainActivity.java
@@ -1,5 +1,8 @@
 package com.bitlyclient;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -10,6 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends ReactActivity {
+    private static final String SHARED_URL_KEY = "sharedUrl";
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -39,5 +43,32 @@ public class MainActivity extends ReactActivity {
             new MainReactPackage(),
             new RNSharePackage()
         );
+    }
+
+    @Override
+    protected Bundle getLaunchOptions() {
+        final Bundle bundle = new Bundle();
+        final String sharedUrl = getSharedText();
+
+        if (sharedUrl != null) {
+            bundle.putCharSequence(SHARED_URL_KEY, sharedUrl);
+        }
+
+        return bundle;
+    }
+
+    private String getSharedText() {
+        final Intent intent = getIntent();
+        final String action = intent.getAction();
+        final String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                final String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+                return sharedText;
+            }
+        }
+
+        return null;
     }
 }
