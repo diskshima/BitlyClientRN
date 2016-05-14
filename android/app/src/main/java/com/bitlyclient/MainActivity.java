2c61e9af0e35c7b724b51cffb296bd4a04255ac4
Daisuke Shimamoto <diskshima@gmail.com>
2016-05-14 16:18:06 +0900

    Update react-native to 0.25.1

--- android/app/src/main/java/com/bitlyclient/MainActivity.java
+++ android/app/src/main/java/com/bitlyclient/MainActivity.java
@@ -1,109 +1,40 @@
 package com.bitlyclient;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.KeyEvent;
-
-import cl.json.RNSharePackage;
-
-import com.facebook.react.LifecycleState;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactRootView;
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 
-import com.diskshima.LinkPackage;
+import java.util.Arrays;
+import java.util.List;
 
-public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private static final String SHARED_URL_KEY = "sharedUrl";
+public class MainActivity extends ReactActivity {
 
-    private ReactInstanceManager mReactInstanceManager;
-    private ReactRootView mReactRootView;
-
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mReactRootView = new ReactRootView(this);
-
-        mReactInstanceManager = ReactInstanceManager.builder()
-                .setApplication(getApplication())
-                .setBundleAssetName("index.android.bundle")
-                .setJSMainModuleName("index.android")
-                .addPackage(new MainReactPackage())
-                .addPackage(new LinkPackage())
-                .addPackage(new RNSharePackage())
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
-                .setInitialLifecycleState(LifecycleState.RESUMED)
-                .build();
-
-        final Bundle bundle = new Bundle();
-        final String sharedUrl = getSharedText();
-
-        if (sharedUrl != null) {
-            bundle.putCharSequence(SHARED_URL_KEY, sharedUrl);
-        }
-
-        mReactRootView.startReactApplication(mReactInstanceManager, "BitlyClient", bundle);
-
-        setContentView(mReactRootView);
+    protected String getMainComponentName() {
+        return "BitlyClient";
     }
 
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
     @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
-            mReactInstanceManager.showDevOptionsDialog();
-            return true;
-        }
-        return super.onKeyUp(keyCode, event);
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
     }
 
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
     @Override
-    public void invokeDefaultOnBackPressed() {
-      super.onBackPressed();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onPause();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onResume(this);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onBackPressed();
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    private String getSharedText() {
-        final Intent intent = getIntent();
-        final String action = intent.getAction();
-        final String type = intent.getType();
-
-        if (Intent.ACTION_SEND.equals(action) && type != null) {
-            if ("text/plain".equals(type)) {
-                final String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
-                return sharedText;
-            }
-        }
-
-        return null;
+    protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage()
+        );
     }
 }
